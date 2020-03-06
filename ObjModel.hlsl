d3529be6c49f91b875ee33b9d287db619255ca8b
371987940 <371987940@qq.com>
2020-03-06 17:53:58 +0800

    111

--- ObjModel.hlsl
+++ ObjModel.hlsl
@@ -6,6 +6,11 @@ cbuffer globalmatrix
 	matrix proj;
 	matrix matviewproj;
 };
+cbuffer lightinfo
+{
+	float4 lightPos;
+	float4 lightcolor;
+};
 cbuffer material
 {
 	float3 ambient;
@@ -22,18 +27,35 @@ struct vs_input
 	float2 texcoord; TEXCOORD;
 }
 
-struct vs_output
+struct ps_input
 {
 	float4 postion:SV_POSITION;
 	float3 normal:NORMAL;
+	float3 color:COLOR;
 	float2 texcoord:TEXCOORD;
 };
-vs_output vs_main(vs_input input)
+ps_input vs_main(vs_input input)
 {
-	vs_output;
-	output.postion = mul(input.positon, matviewproj);
-	output.normal = normal;
+	ps_input output;
+	output.postion = mul(input.positon, world);
+	output.postion = mul(output.postion, matviewproj);
+	output.normal =  mul(normal,(float3x3)world);
 	output.texcoord = input.texcoord;
+
+	output.vColor.rgb = g_vLightColor * g_vMaterialAmbient;
+	output.vColor.rgb += g_vLightColor * g_vMaterialDiffuse * saturate(dot(vLight, vNormalWorld));
+
+	// If enabled, calculate the specular term
+	if (bSpecular)
+	{
+		float3 vCamera = normalize(vPosWorld.xyz - g_vCameraPosition);
+		float3 vReflection = reflect(vLight, vNormalWorld);
+		float  fPhoneValue = saturate(dot(vReflection, vCamera));
+
+		output.vColor.rgb += g_vMaterialSpecular * pow(fPhoneValue, g_nMaterialShininess);
+	}
+
+	output.vColor.a = g_fMaterialAlpha;
 }
 float4 ps_main( vs_output psInput ):SV_Target
 {
