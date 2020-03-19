ea7eff9755de47fb9d24bd8d42be05d07233fbbd
371987940 <371987940@qq.com>
2020-03-19 19:54:27 +0800

    11111

--- GLTF.cpp
+++ GLTF.cpp
@@ -41,12 +41,19 @@ bool CGLTF::Init(ID3D11Device* pd3dDevice, ID3D11DeviceContext* pContext )
 	m_TransMatrixBuffer = std::shared_ptr<ID3D11Buffer>(CreateBuffer(m_pd3dDevice, sizeof(globalmatrix), D3D11_USAGE_DEFAULT, D3D11_BIND_CONSTANT_BUFFER, 0, 0) , DeleteComPtr ) ;
 	m_LightInfoBuffer = std::shared_ptr<ID3D11Buffer>(CreateBuffer(m_pd3dDevice, sizeof(lightinfo), D3D11_USAGE_DEFAULT, D3D11_BIND_CONSTANT_BUFFER, 0, 0) , DeleteComPtr );
 	m_MaterialBuffer = std::shared_ptr<ID3D11Buffer>(CreateBuffer(m_pd3dDevice, sizeof(material), D3D11_USAGE_DEFAULT, D3D11_BIND_CONSTANT_BUFFER, 0, 0), DeleteComPtr);
-
+	D3D11_INPUT_ELEMENT_DESC elements[]=
+	{
+		"POSITION",
+	};
+	pd3dDevice->CreateInputLayout()
 	return true;
 }
 bool CGLTF::Render(DWORD dwTimes)
 {
 	//Update
-	
+	for (auto& Node : m_pGLTFModel->Nodes)
+	{
+
+	}
 	return false;
 }
